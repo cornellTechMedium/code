bc4d9f40cf83b461c571572e0d80b860fd008375
Adam Gavish <agavish@gmail.com>
2016-09-26 09:16:53 -0400

    coupled pairs of challenges

--- public/modules/tabs/controllers/tabsCtrl.js
+++ public/modules/tabs/controllers/tabsCtrl.js
@@ -1,24 +1,33 @@
+var FIRST_COMPANY_INDEX = 0;
+var SECOND_COMPANY_INDEX = 1;
+
 angular.module('tabsCtrl', ['tabsService', 'cardsService'])
 .controller('tabsCtrl', function(Tabs, Cards) {
 	
 	self = this;
 
 	// Grab all the items from Tabs Service
-	Tabs.all()
-        .success(function(data) {
-            self.challenges = data;
-        })
-        .error(function(data) {
-            // Error Handling
-        });
-
     Cards.all()
         .success(function(data) {
             self.companies = data;
+
+            Tabs.all()
+                .success(function(data) {
+                    self.challenges = data;
+
+                    self.challenges.list.forEach(function(challenge) {
+                        challenge.companies[FIRST_COMPANY_INDEX]["company"] = self.companies.list[challenge.companies[FIRST_COMPANY_INDEX].id-1];
+                        challenge.companies[SECOND_COMPANY_INDEX]["company"] = self.companies.list[challenge.companies[SECOND_COMPANY_INDEX].id-1];
+                    });
+
+                    console.log(self.challenges.list);
+                })
+                .error(function(data) {
+                    // Error Handling
+                });
+
         })
         .error(function(data) {
             // Error Handling
         });
-
-
 });
