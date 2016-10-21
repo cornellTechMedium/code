dcd05df49ccae86e2599ee8a05e6aa5936e3b326
Zephyr Liu <youxiang518@gmail.com>
2016-10-21 11:18:51 -0400

    merged from last night, flatten bug fixed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var kgsearchapi = googleapi.kgsearch('v1');
 
 // Google Youtube
 var youTube = new YouTube();
-youTube.setKey('IzaSyDK-lenDCs83cEvFTknrvpINEHnZCSOcIg');
+youTube.setKey('AIzaSyDK-lenDCs83cEvFTknrvpINEHnZCSOcIg');
 
 // Twitter search
 var twitterSearchClient = new Twitter.SearchClient(
@@ -32,7 +32,6 @@ var twitterSearchClient = new Twitter.SearchClient(
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
@@ -51,18 +50,21 @@ app.post('/detect', function (req, res) {
         verbose: true
     };
 
-    return new Promise(function (resolve, reject) {
-        language.detectEntities(paragraph, options, function (err, data) {
-            if (err) {
-                reject(err);
-            } else {
-                console.log(data);
-                var results = _.flattenDeep([data.people, data.places, data.organizations]);
-                resolve(results);
-            }
-        });
-    })
-        .then(function (entities) {
+    return new Promise(function(resolve, reject) {
+            language.detectEntities(paragraph, options, function(err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    var categories = [];
+                    for (var i in Object(data)) {
+                        categories.push(Object(data)[i]);
+                    }
+                    console.log(categories);
+                    resolve(_.flattenDeep(categories));
+                }
+            });
+        })
+        .then(function(entities) {
             var promises = [];
             // var entities = _.flattenDeep([data.entities.people, data.entities.places, data.entities.organizations]);
 
@@ -93,18 +95,17 @@ app.get('/*', function (req, res, next) {
     res.sendFile('index.html', {root: __dirname + '/public'});
 });
 
-var searchGoogle = function (entity) {
-    return new Promise(function (resolve, reject) {
-
-        google(entity.name, 1, function (err, data) {
-            entity["link"] = data;
+var searchGoogle = function(entity) {
+    return new Promise(function(resolve, reject) {
+        google(entity.name, 1, function(err, data) {
+            entity["googleSearch"] = data;
             resolve(entity);
         });
     });
 };
 
-var searchGoogleImages = function (entity) {
-    return new Promise(function (resolve, reject) {
+var searchGoogleImages = function(entity) {
+    return new Promise(function(resolve, reject) {
         client.search(entity.name)
             .then(function(data) {
                 entity["googleImages"] = data;
@@ -116,7 +117,6 @@ var searchGoogleImages = function (entity) {
 var searchYoutube = function(entity) {
     return new Promise(function(resolve, reject) {
         youTube.search(entity.name, 1, function(error, result) {
-            // console.log(JSON.stringify(result, null, 2));
             entity["youtube"] = result;
             resolve(entity);
         });
@@ -148,9 +148,8 @@ var searchWiki = function(entity) {
         };
         kgsearchapi.entities.search(params, function(err, response) {
             if (err) {
-                console.log('Encountered error', err);
+                console.log('Error: ', err);
             } else {
-                // console.log(JSON.stringify(response, null, 2));
                 entity["wikipedia"] = response;
                 resolve(entity);
             }
