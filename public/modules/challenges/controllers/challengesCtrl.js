2aee8305f082eab7f6a0615591bee7b4329d0339
brokenmouse <youxiang518@gmail.com>
2016-11-18 13:43:28 -0500

    Drag and drop

--- public/modules/challenges/controllers/challengesCtrl.js
+++ public/modules/challenges/controllers/challengesCtrl.js
@@ -1,13 +1,13 @@
 var module = angular.module('challengesCtrl', ['challengesService', 'companiesService']);
 
-module.controller('challengesCtrl', function(Tabs, Cards, $compile, $http, $sce, $scope, $stateParams, $rootScope, $timeout, $mdSidenav) {
-
+module.controller('challengesCtrl', function(Tabs, Cards, $compile, $http, $sce, $scope, $stateParams, $rootScope, $timeout, $mdSidenav, dragularService) {
+    dragularService('.containerVertical');
     self = this;
 
     // Side bar controller
     $scope.toggleLeft = buildToggler('left');
     $scope.toggleRight = buildToggler('right');
-	$scope.openLeft = buildTogglerOpen('left');
+    $scope.openLeft = buildTogglerOpen('left');
 
     function buildToggler(componentId) {
         return function() {
@@ -15,11 +15,11 @@ module.controller('challengesCtrl', function(Tabs, Cards, $compile, $http, $sce,
         }
     }
 
-	function buildTogglerOpen(componentId) {
-		return function() {
-			$mdSidenav(componentId).open();
-		}
-	}
+    function buildTogglerOpen(componentId) {
+        return function() {
+            $mdSidenav(componentId).open();
+        }
+    }
     // Side bar controller
 
     $scope.result = '<p>' + $rootScope.request.text + '</p>';
@@ -27,23 +27,21 @@ module.controller('challengesCtrl', function(Tabs, Cards, $compile, $http, $sce,
     $scope.openTooltip = function(event) {
 
 
-        $scope.tooltip = '<md-content layout-padding>' +
-            '<md-grid-list md-cols="1" md-row-height="fit" style="height: 400px;">';
+        $scope.tooltip = '<div class="wrapper" ng-controller="challengesCtrl"><div class="containerVertical">';
 
         var entity = $scope.data.hashmap[event.target.innerText];
 
         entity.googleImages.forEach(function(image) {
-            $scope.tooltip += '<md-grid-tile md-rowspan="1" md-colspan="1" md-colspan-sm="1" md-colspan-xs="1">' +
-                '<img class="image-responsive" src="' + image.url + '"/>' +
-                '</md-grid-tile>';
+            $scope.tooltip += '<div><img class="image-responsive containerVertical" src="' + image.url + '"/>' +
+                '</md-grid-tile></div>';
         });
-        $scope.tooltip += '</md-grid-list></md-content>';
+        $scope.tooltip += '</div>';
 
-        $scope.wikipedia = '';
+        $scope.wikipedia = '<md-content layout-margin class="containerVertical"><p>';
 
         $scope.wikipedia += entity.wikipedia.itemListElement[0].result.detailedDescription.articleBody;
 
-        $scope.wikipedia += '</p>';
+        $scope.wikipedia += '</p></md-content>';
 
         $scope.youtube = '';
 
